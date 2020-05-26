6e93af84ad01ecf2c7f00545c3f9aff595b9d2f3
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-05-26 19:29:18 +0200

    should display 1 when parameter is 1

--- FizzBuzz/src/test/scala/FizzBuzzTest.scala
+++ FizzBuzz/src/test/scala/FizzBuzzTest.scala
@@ -1,6 +1,12 @@
 import org.scalatest.{FunSpec, Matchers}
 
-object FizzBuzzTest extends FunSpec with Matchers {
+class FizzBuzzTest extends FunSpec with Matchers {
 
   val fizzBuzz = new FizzBuzz
+
+  describe("FizzBuzz tests") {
+    it("should display 1 when parameter is 1") {
+      fizzBuzz.display(1) should be ("1")
+    }
+  }
 }
