0730e91b1b2013a8ae838bd0863bdf7077b02115
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-05-26 19:59:34 +0200

    Extract constants

--- FizzBuzz/src/main/scala/FizzBuzz.scala
+++ FizzBuzz/src/main/scala/FizzBuzz.scala
@@ -1,8 +1,12 @@
 class FizzBuzz {
+  val FIZZ: String= "Fizz"
+  val BUZZ: String = "Buzz"
+  val FIZZBUZZ: String = "FizzBuzz"
+
   def display(i: Int): String = i match {
-    case x if x % 3 == 0 && x % 5 == 0 => "FizzBuzz"
-    case x if x % 3 == 0 => "Fizz"
-    case x if x % 5 == 0 => "Buzz"
+    case x if x % 3 == 0 && x % 5 == 0 => FIZZBUZZ: String
+    case x if x % 3 == 0 => FIZZ
+    case x if x % 5 == 0 => BUZZ
     case x => x.toString
   }
 }
