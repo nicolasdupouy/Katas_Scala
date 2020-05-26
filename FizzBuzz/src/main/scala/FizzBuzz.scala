15176c2eda1410b0cbafa46f7c6bb0906253a862
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-05-26 20:04:55 +0200

    Extract methods is multiple of

--- FizzBuzz/src/main/scala/FizzBuzz.scala
+++ FizzBuzz/src/main/scala/FizzBuzz.scala
@@ -1,12 +1,18 @@
 class FizzBuzz {
   val FIZZ: String= "Fizz"
   val BUZZ: String = "Buzz"
-  val FIZZBUZZ: String = "FizzBuzz"
+  val FIZZ_BUZZ: String = "FizzBuzz"
 
-  def display(i: Int): String = i match {
-    case x if x % 3 == 0 && x % 5 == 0 => FIZZBUZZ: String
-    case x if x % 3 == 0 => FIZZ
-    case x if x % 5 == 0 => BUZZ
-    case x => x.toString
+  def display(i: Int): String = {
+    def isMultipleOfThreeAndFive(x: Int): Boolean = isMultipleOfThree(x) && isMultipleOfFive(x)
+    def isMultipleOfThree(x: Int): Boolean = x % 3 == 0
+    def isMultipleOfFive(x: Int): Boolean = x % 5 == 0
+
+    i match {
+      case x if isMultipleOfThreeAndFive(x) => FIZZ_BUZZ: String
+      case x if isMultipleOfThree(x) => FIZZ
+      case x if isMultipleOfFive(x) => BUZZ
+      case x => x.toString
+    }
   }
 }
