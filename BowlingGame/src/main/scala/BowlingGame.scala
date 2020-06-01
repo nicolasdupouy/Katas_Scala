d026878eb769499a78d1dd97a69871a2b2399d83
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-06-01 09:06:09 +0200

    Suppress use of Nil

--- BowlingGame/src/main/scala/BowlingGame.scala
+++ BowlingGame/src/main/scala/BowlingGame.scala
@@ -4,8 +4,8 @@ class BowlingGame {
   def roll(pins: Int): Unit = {
     if (
       frames.nonEmpty
-      && frames.last.roll2 == Nil)
-      frames.last.roll2 = pins
+      && frames.last.isNotFinished)
+      frames.last.setRoll2(pins)
     else
       frames = frames :+ Frame(pins)
   }
@@ -13,9 +13,9 @@ class BowlingGame {
   def score(): Int = {
     var computedScore: Int = 0
     for (frame <- frames) {
-      computedScore += frame.roll1
-      if (frame.roll2 != Nil)
-        computedScore += frame.roll2
+      computedScore += frame.getRoll1
+      if (frame.isFinished)
+        computedScore += frame.getRoll2
     }
     computedScore
   }
--- BowlingGame/src/main/scala/Frame.scala
+++ BowlingGame/src/main/scala/Frame.scala
@@ -1,2 +1,16 @@
-case class Frame(roll1: Int, var roll2: Int = 0) {
+case class Frame(private val pins: Int) {
+  private val roll1: Int = pins
+  private var roll2: Int = 0
+  private var finished = false
+
+  def getRoll1: Int = roll1
+  def getRoll2: Int = roll2
+
+  def setRoll2(roll2: Int): Unit = {
+    this.roll2 = roll2
+    this.finished = true
+  }
+
+  def isFinished: Boolean = finished
+  def isNotFinished: Boolean = !finished
 }
