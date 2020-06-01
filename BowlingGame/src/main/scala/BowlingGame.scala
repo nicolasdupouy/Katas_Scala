fabc1db91a47ef310ce1afa2803e5be94a3e7d5d
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-06-01 09:32:46 +0200

    Refactor with recursivity

--- BowlingGame/src/main/scala/BowlingGame.scala
+++ BowlingGame/src/main/scala/BowlingGame.scala
@@ -1,22 +1,24 @@
+import scala.annotation.tailrec
+
 class BowlingGame {
   private var frames = List[Frame]()
 
   def roll(pins: Int): Unit = {
     if (
       frames.nonEmpty
-      && frames.last.isNotFinished)
+        && frames.last.isNotFinished)
       frames.last.setRoll2(pins)
     else
       frames = frames :+ Frame(pins)
   }
 
   def score(): Int = {
-    var computedScore: Int = 0
-    for (frame <- frames) {
-      computedScore += frame.getRoll1
-      if (frame.isFinished)
-        computedScore += frame.getRoll2
+    @tailrec
+    def loop(frames: List[Frame], computedScore: Int): Int = {
+      if (frames.isEmpty) computedScore
+      else loop(frames.tail, computedScore + frames.head.getRoll1 + frames.head.getRoll2)
     }
-    computedScore
+
+    loop(this.frames, 0)
   }
 }
