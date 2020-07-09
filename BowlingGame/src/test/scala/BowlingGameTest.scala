d1ad6b1481a71cf6e40b6f4c26304f638205df4e
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-07-09 09:33:05 +0200

    Test perfect game

--- BowlingGame/src/test/scala/BowlingGameTest.scala
+++ BowlingGame/src/test/scala/BowlingGameTest.scala
@@ -1,16 +1,25 @@
 import org.scalatest.{FunSpec, Matchers}
 
 class BowlingGameTest extends FunSpec with Matchers {
-  val game = new BowlingGame
-
   describe("Bowling game") {
     it("should score 0 if all frames are failed") {
-      for(i <- 1 to 20) {
+      val game = new BowlingGame
+      for(_ <- 1 to 20) {
         game.roll(0)
       }
 
       game.scorePatternMatching should be(0)
       game.scoreRecursive should be(0)
+    }
+
+    it("should score 300 for a perfect game") {
+      val game = new BowlingGame
+      for(_ <- 1 to 12) {
+        game.roll(10)
+      }
+
+      game.scorePatternMatching should be(300)
+      game.scoreRecursive should be(300)
     }
   }
 }
